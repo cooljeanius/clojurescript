f8123856469f66874c75624c9bdb985988ad1ac9
Eric Gallager <egall@gwmail.gwu.edu>
2023-11-27 21:32:28 -0500

    oops, I missed a few

--- ci/install_jsc.sh
+++ ci/install_jsc.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+set -x
 if [ ! -d WebKit ]
 then
   git clone -b Safari-611.1.5.1 --depth=1 https://github.com/WebKit/WebKit.git WebKit;
-  cd WebKit;
+  pushd WebKit || exit;
   Tools/Scripts/build-jsc --jsc-only;
-  cd ..
+  popd || exit;
 fi
