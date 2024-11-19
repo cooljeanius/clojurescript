f3d20671f11d67dff5f5bfde5640bad6b46cc231
Eric Gallager <egall@gwmail.gwu.edu>
2024-11-19 12:16:56 -0500

    a few things noticed when browsing in VSCode

--- ast-ref/buildref.sh
+++ ast-ref/buildref.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
-java -cp .:`lein cp` clojure.main <<EOF
+java -cp .:"$(lein cp)" clojure.main <<EOF
  (load "gen-ref")
  (System/exit 0)
 EOF
 
-#git pull
-#mv quickref.html q.html
-#git checkout origin/gh-pages
-#mv q.html quickref.html
-#git add quickref.html
-#git commit -m "update quickref"
-#git push origin HEAD:gh-pages
-#git checkout master
+if test -n "${DO_COMMENTED_OUT_JUNK}" && test "${DO_COMMENTED_OUT_JUNK}" = "YES"; then
+  if test -x "$(which git)" && test -d .git && test -r .git; then
+    git pull
+    if test -r quickref.html && test ! -e q.html && test -w .; then
+      mv quickref.html q.html
+    fi
+    git checkout origin/gh-pages
+    if test -r q.html && test ! -e q.html && test -w .; then
+      mv q.html quickref.html
+    fi
+    if test -r quickref.html; then
+      git add quickref.html
+      git commit -m "update quickref"
+    fi
+    git push origin HEAD:gh-pages
+    git checkout master
+  fi
+fi
